c4a8816e63444ff02476c475d73bfa9ef2120c3f
maainul <mainul080@yahoo.com>
2024-03-19 08:46:10 +0600

    Validation Added

--- validation/VehicleValidation.js
+++ validation/VehicleValidation.js
@@ -7,23 +7,24 @@ const vehicleSchema = Joi.object({
         color: Joi.string().required(),
         model: Joi.string().required(),
         numberPlate: Joi.string().required()
-    }),
+    }).required(),
     drivers: Joi.array().items(Joi.object({
         name: Joi.string().required(),
         photo: Joi.string(),
         address: Joi.string(),
         phoneNumber: Joi.string().required()
-    })),
+    })).required(),
     visitors: Joi.array().items(Joi.object({
         referencePeople: Joi.array().items(Joi.object({
             name: Joi.string().required()
-        })),
+        })).required(),
         numberOfPassengers: Joi.number().required(),
         purpose: Joi.string().required(),
-        entryTimes: Joi.date().required() // Add this line to include entryTimes in validation
-    }))
+        entryTimes: Joi.date() // Add this line to include entryTimes in validation
+    })).required()
 });
 
 export const validateVehicle = (data) => {
-    return vehicleSchema.validate(data);
+    const options = { abortEarly: false, allowUnknown: false }
+    return vehicleSchema.validate(data, options);
 };
